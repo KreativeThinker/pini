64a3eaf206ac438b255fe4827a422508b3fb3ffe
KreativeThinker <sagely2005eagle@gmail.com>
2025-06-05 12:05:35 +0530

    feat(stacks): add django init

---
+++ stacks/django.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+PROJECT_NAME=$1
+
+mkdir "$PROJECT_NAME"
+cd "$PROJECT_NAME"
+
+uv venv
+source .venv/bin/activate
+
+uv pip install django
+django-admin startproject core .
+
+git init
+cz init
+
+# Add config copies later
