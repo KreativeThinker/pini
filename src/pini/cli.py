ced1eb3113ccc8acfcd1eb5ccec53d13db3fa553
KreativeThinker <sagely2005eagle@gmail.com>
2025-07-11 23:32:26 +0530

    fix: git add after pre-commit

--- src/pini/cli.py
+++ src/pini/cli.py
@@ -207,6 +207,7 @@ def create():
         subprocess.run(["git", "add", "."], cwd=project_name, check=True)
         if init_pre_commit_hooks:
             subprocess.run("pre-commit", "run", cwd=project_name, check=True)
+            subprocess.run(["git", "add", "."], cwd=project_name, check=True)
         subprocess.run(
             ["git", "commit", "-m", "Initialized with PIni"],
             cwd=project_name,
